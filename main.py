adbd42c5f77a5881a63ceb390ddf809b31785b35
Johnathon <john.rohweder1015@gmail.com>
2022-03-25 22:02:58 -0700

    Switched from list to string

--- main.py
+++ main.py
@@ -7,12 +7,19 @@ class monkey():
     
     def __init__(self):
         self.letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-        self.collection = []
+        self.collection = ''
     
     def main(self):
         while True:
-            self.collection.append(self.letters[random.randint(0, 25)])
+            self.addLetter()
             print(self.collection)
+            
+    def addLetter(self):
+        self.collection += self.letters[random.randint(0, 25)]
+        
+    def wordIdentifier(self):
+        pass
+    
 
 
 if __name__ == '__main__':
