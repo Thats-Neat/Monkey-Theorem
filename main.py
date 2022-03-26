204c81d55534313758ff4cc8d6f7716a2c356ff7
Johnathon <john.rohweder1015@gmail.com>
2022-03-25 21:56:38 -0700

    Add letters to array

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 # Monkey Theorem
 # using simplified keyboard. A-Z
 
+import random
+
 class monkey():
     
     def __init__(self):
-        letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+        self.letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+        self.collection = []
     
     def main(self):
-        pass
+        while True:
+            self.collection.append(self.letters[random.randint(0, 25)])
+            print(self.collection)
 
 
 if __name__ == '__main__':
