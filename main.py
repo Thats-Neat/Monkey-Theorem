12de554fe304e9cb5541048a21db9dd46c10ae14
Johnathon <john.rohweder1015@gmail.com>
2022-03-25 21:52:19 -0700

    first commit

---
+++ main.py
@@ -0,0 +1,15 @@
+# Monkey Theorem
+# using simplified keyboard. A-Z
+
+class monkey():
+    
+    def __init__(self):
+        letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+    
+    def main(self):
+        pass
+
+
+if __name__ == '__main__':
+    main = monkey()
+    main.main()
